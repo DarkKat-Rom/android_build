061d0a676d11c50a07a8945035c7e27ebf5b4119
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-08-25 22:06:37 +0200

    Add bacon target, and mka/brunch/breakfast aliases, (1/2):
    (1/2): android_build
    (2/2): android_vendor_dk
    
    Based on OmniRom::
    https://github.com/omnirom/android_build/commit/5e43076ec1a001e80dfd801c7e4bc35c41f64722
    
    Darkkat additions:
    - Add default Darkkat build type: "Test"
    - Add custom Darkkat build type (optional)
    
    To set a custom Darkkat build type use the brunch commands second parameter,
    e.g. brunch hammerhead My-Custom-Build-Type

--- envsetup.sh
+++ envsetup.sh
@@ -129,6 +129,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^dk_") ; then
+        DARKKAT_BUILD=$(echo -n $1 | sed -e 's/^dk_//g')
+    else
+        DARKKAT_BUILD=
+    fi
+    export DARKKAT_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -560,6 +568,69 @@ function print_lunch_menu()
     done
 
     echo
+}
+
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+time mka bacon
+    else
+echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+return $?
+}
+
+function breakfast()
+{
+    target=$1
+    local type="TEST"
+    local customtype=$2
+    if [ -z "$customtype" ]; then
+        export DK_BUILD_TYPE=$type
+    else
+        export DK_BUILD_TYPE=$customtype
+    fi
+    DK_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/darkkat/vendorsetup.sh 2> /dev/null`
+        do
+echo "including $f"
+            . $f
+        done
+unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the darkkat model name
+            lunch dk_$target-userdebug
+        fi
+fi
+return $?
+}
+
+alias bib=breakfast
+
+# Make using all available CPUs
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            ;;
+    esac
 }
 
 function lunch()
