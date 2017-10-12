ab9ec03f1e5858a4632a5a2bc70ae0fb26e8b19a
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-10-12 13:21:57 +0200

    Kill schedtool

--- envsetup.sh
+++ envsetup.sh
@@ -623,14 +623,7 @@ alias bib=breakfast
 
 # Make using all available CPUs
 function mka() {
-    case `uname -s` in
-        Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-            ;;
-        *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
-            ;;
-    esac
+    m -j "$@"
 }
 
 function lunch()
